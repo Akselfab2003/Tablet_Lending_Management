9dc64c043108328cab03ae9d7b92b598ace9f522
Aksel Fabricius <akselfabricius@gmail.com>
2025-01-23 19:26:47 +0100

    updated ui

--- app/src/main/java/com/example/tablet_lending_management/MainActivity.java
+++ app/src/main/java/com/example/tablet_lending_management/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -17,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Added Dark mode to follow system preferences
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
